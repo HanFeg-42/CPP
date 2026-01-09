b4b78a2a586703dce7a2fd8dfabaca900a3dea91
HanFeg-42 <hananefegrachi2002@gmail.com>
2026-01-09 11:17:24 +0100

    create cpp05/ex00 files

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+    std::string const _name;
+    int _grade;
+public:
+    Bureaucrat(/* args */);
+    ~Bureaucrat();
+};
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
