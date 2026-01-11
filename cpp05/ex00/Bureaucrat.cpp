3f70355235c8f21bf96a7059043a42b981c6fb12
HanFeg-42 <hananefegrachi@gmail.com>
2026-01-11 10:45:02 +0100

    start ex00/cpp05

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,34 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+Bureaucrat::Bureaucrat(const Bureaucrat & other)
+{
+
+}
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat & other)
+{
+
+}
+
+std::string const Bureaucrat::getName() const
+{
+
+}
+int     Bureaucrat::getGrade() const
+{
+
+}
+void    Bureaucrat::incrementGrade() const
+{
+    
+}
+void    Bureaucrat::decrementGrade() const
+{
+    
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -8,14 +8,14 @@ private:
     std::string const _name;
     int _grade;
 public:
-    Bureaucrat(/* args */);
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat & other);
+    Bureaucrat & operator=(const Bureaucrat & other);
     ~Bureaucrat();
+
+    std::string const getName() const;
+    int     getGrade() const;
+    void    incrementGrade() const;
+    void    decrementGrade() const;
 };
 
-Bureaucrat::Bureaucrat(/* args */)
-{
-}
-
-Bureaucrat::~Bureaucrat()
-{
-}
