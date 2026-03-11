6239ba14726f0bf46edca18ee74d8f566dceae29
HanFeg-42 <hananefegrachi2002@gmail.com>
2026-03-11 16:07:47 +0000

    add the files for ex02/cpp06

---
+++ cpp06/ex02/A.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "Base.hpp"
+class A : public Base
+{
+private:
+    /* data */
+public:
+    A(/* args */);
+    ~A();
+};
+
+A::A(/* args */)
+{
+}
+
+A::~A()
+{
+}
---
+++ cpp06/ex02/C.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "Base.hpp"
+class C : public Base
+{
+private:
+    /* data */
+public:
+    C(/* args */);
+    ~C();
+};
+
+C::C(/* args */)
+{
+}
+
+C::~C()
+{
+}
