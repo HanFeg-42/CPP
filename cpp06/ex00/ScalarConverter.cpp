94f6155d87b7ef01069d001f9a499b9f06e086bb
HanFeg-42 <hananefegrachi2002@gmail.com>
2026-03-20 12:05:25 +0000

    almost finish ex00/cpp06

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,41 +3,107 @@
 #include <iomanip>
 #include <sstream>
 
+enum Type {
+    CHAR,
+    INT,
+    FLOAT,
+    DOUBLE,
+    PseudoLiteral
+};
+
 ScalarConverter& ScalarConverter::operator=(ScalarConverter& other)
 {
     (void)other;
     return *this;
 }
 
+const char* ScalarConverter::InvalidConversionException::what() const throw()
+{
+	return "Invalid Conversion!";
+}
+
+int isPseudoLiteral(std::string& s)
+{
+    return (s == "-inff" || s == "+inff" || s == "nanf"
+            || s == "nan" || s == "-inf" || s == "+inf");
+}
+
 char getType(std::string& s)
 {
-    (void)s;
-    return 'i';
+    if (s == "-inff" || s == "+inff" || s == "nanf")
+        return FLOAT;
+    if (s == "nan" || s == "-inf" || s == "+inf")
+        return DOUBLE;
+    std::stringstream ss(s);
+    if (s.length() == 1 && !isdigit(s[0]))
+        return CHAR;
+    return INT;
 }
 
-int toInt(std::string& s)
+void toInt(double val, std::string& s)
 {
+    if (isPseudoLiteral(s))
+        std::cout << "int: impossible\n";
+    else
+        std::cout << "int: " << static_cast<int>(val) << "\n";
+}
+void toDouble(double val, int type, std::string s)
+{
+    if (isPseudoLiteral(s))
+    {
+        if (type == DOUBLE)
+            std::cout << "double: " << s << "\n";
+        else
+            std::cout << "double: " << s.substr(0, s.size() - 1) << "\n";
+        return ;
+    }
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "double: " << val << "\n";
+}
+void toFloat(double val, int type, std::string s)
+{
+    if (isPseudoLiteral(s))
+    {
+        if (type == FLOAT)
+            std::cout << "float: " << s << "\n";
+        else
+            std::cout << "float: " << s + "f" << "\n";
+        return ;
+    }
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "float: " << static_cast<float>(val) << "f\n";
 
 }
-double toDouble(std::string& s)
+void toChar(double val, std::string& s)
 {
-
-}
-float toFloat(std::string& s)
-{
-
-}
-char toChar(std::string& s)
-{
+    if (isPseudoLiteral(s))
+        std::cout << "char: impossible\n";
+    else if (val > 126 || val < 32)
+        std::cout << "char: Non displayable\n";
+    else
+        std::cout << "char: '" << static_cast<char>(val) << "'\n";
 
 }
 
 void ScalarConverter::convert(std::string literal)
 {
-    toChar(literal);
-    toInt(literal);
-    toFloat(literal);
-    toDouble(literal);
+    std::stringstream ss(literal);
+    double value = 0.0;
+    char c;
+    
+    int type = getType(literal);
+    if (!isPseudoLiteral(literal) && type != CHAR)
+    {
+        ss >> value;
+        if (ss.fail() || (ss >> c && c != 'f') || ss >> c)
+            throw InvalidConversionException();
+    }
+    if (type == CHAR)
+        value = static_cast<double>(literal[0]);
+    toChar(value, literal);
+    toInt(value, literal);
+    toFloat(value, type, literal);
+    toDouble(value, type, literal);
 }
 
 ScalarConverter::~ScalarConverter() {}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -8,6 +8,10 @@ class ScalarConverter
     ScalarConverter(ScalarConverter& other);
     ScalarConverter& operator=(ScalarConverter& other);
 public:
+	class InvalidConversionException : public std::exception {
+	public:
+		const char* what() const throw();
+	};
     static void convert(std::string literal);
     ~ScalarConverter();
 };
