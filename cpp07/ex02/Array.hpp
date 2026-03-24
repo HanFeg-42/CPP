49a633467d6d345d88d19b916e35086afa69b3aa
HanFeg-42 <hananefegrachi2002@gmail.com>
2026-03-24 09:57:10 +0100

    add ex02/cpp07, version: hpp only

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,15 +1,59 @@
 #pragma once
 
+#include <iostream>
+#include <exception>
+
 template<class T>
 class Array
 {
 private:
     T* data;
-    unsigned int size;
+    unsigned int _size;
 public:
-    Array() : size(0), data(NULL) {}
-    Array(unsigned int n) : size(n) {
-        data = new T[size];
+    Array() : _size(0), data(NULL) {}
+    Array(unsigned int n) : _size(n) {
+        data = new T[_size];
+    }
+    Array(const Array& other) : _size(other._size) {
+        data = new T[_size];
+        for (unsigned int i = 0; i < _size; i++)
+            data[i] = other.data[i];
+    }
+    Array & operator=(const Array& other) {
+        if (this != &other)
+        {
+            // kayna chii haaja hna what if the allocation failed
+            _size = other._size;
+            if (data)
+                delete[] data;
+            data = new T[_size];
+            for (unsigned int i = 0; i < _size; i++)
+                data[i] = other.data[i];    
+        }
+        return *this;
     }
     ~Array() { delete[] data; }
+
+    class NonAccessibleException : public std::exception {
+        public:
+            const char* what() const throw() {
+                return "Non Accessible!";
+            }
+    };
+
+    T& operator[](unsigned int i) {
+        if (i >= _size)
+            throw NonAccessibleException();
+        return data[i];
+    }
+    
+    const T& operator[](unsigned int i) const {
+        if (i >= _size)
+            throw NonAccessibleException();
+        return data[i];
+    }
+
+    unsigned int size() const {
+        return _size;
+    }
 };
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,8 @@
+#include "Array.hpp"
+#include <iostream>
+
+int main()
+{
+    int * a = new int();
+    std::cout << *a << "\n";
+}
