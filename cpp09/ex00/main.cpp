14c9d72436c0d1c8e5a2d333c06ad325e64e8572
HanFeg-42 <hananefegrachi2002@gmail.com>
2026-04-22 17:00:07 +0100

    create files

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char *av[])
+{
+    try
+    {
+        if (ac < 2)
+            throw std::unvalid_argument("Error: could not open file.");
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
